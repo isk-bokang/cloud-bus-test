4128f3a97332af5373d502da201b733936f2dc62
isk-bokang <bo.kang@iskra.world>
2022-08-01 13:33:00 +0900

    initial commit

---
+++ src/main/kotlin/com/example/cloudbustest/CloudBusTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cloudbustest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloudBusTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<CloudBusTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cloudbustest/CloudBusTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cloudbustest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudBusTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
